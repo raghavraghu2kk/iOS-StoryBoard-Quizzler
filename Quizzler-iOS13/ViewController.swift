a03729db0c27aebc96d2f55ccd557b2d1e6673e1
Raghavendra Mirajkar <raghavendramirajkar@Raghavendras-MacBook-Air.local>
2024-03-08 06:22:10 +0530

    Add IBOutlets

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz : [String] = [
+    "Four + Two is equal to Six",
+    "One + Three is equal to Four",
+    "Seven + Eight is equal to Ten"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButton(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
+    
 }
 
